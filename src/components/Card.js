092811d74c74c3c3b37da636f37b8d11f0b9ec02
tushargupte <tushar@TusharsMBP.local>
2018-08-08 15:36:12 +0530

    Copied Card.js code from Design+Code Git

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 const Card = props => (
     <div className="Card">
-        <image src={props.image} />
+        <img src={props.image} />
         <h3>{props.title}</h3>
         <p>{props.text}</p>
     </div>
