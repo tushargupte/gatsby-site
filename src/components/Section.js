799620d5d36e9e87c8113c27b2eaff26c501f4d0
tushargupte <tushar@TusharsMBP.local>
2018-08-11 16:54:45 +0530

    CSS Components - 75% done

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,27 +1,63 @@
 import React from 'react'
 import styled from 'styled-components'
+import Wave from './Wave';
 
 const SectionGroup = styled.div `
     background: url(${props => props.image});
     height: 720px;
     background-size: cover;
     display: grid;
+    grid-template-rows: 300px auto;
+    grid-gap: 20px;
+    position: relative;
 `
 
 const SectionLogo = styled.img `
+    align-self: end;
+    width: 128px;
+    margin: 0 auto;
 `
 
 const SectionTitleGroup = styled.div `
+    display: grid;
+    grid-template-columns: 300px auto;
+    margin: 0 40px;
+    grid-gap: 20px;
+    grid-template-rows: auto 100%;
+
+    @media (max-width: 720px) {
+        grid-template-columns: 1fr;
+    }
 `
 
 const SectionTitle = styled.h3 `
+    color: white;
+    font-size: 50px;
+    line-height: 100%;
+    margin: 0;
 `
 
 const SectionText = styled.p `
+    color: white;
+`
+
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: -6px;
+`
+
+const WaveTop = styled.div`
+    position: absolute;
+    top: -6px;
+    width: 100%;
+    transform: rotate(180deg);
 `
 
 const Section = props => (
     <SectionGroup image={props.image}>
+        <WaveTop><Wave /></WaveTop>
+        <WaveBottom><Wave /></WaveBottom>
         <SectionLogo src={props.logo} />
         <SectionTitleGroup>
             <SectionTitle>{props.title}</SectionTitle>
