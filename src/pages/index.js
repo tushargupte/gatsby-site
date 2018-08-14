90487aae2bb15d40b39ecd7a118f942492ea4c86
tushargupte <tushar@TusharsMBP.local>
2018-08-14 16:55:31 +0530

    Static JSON lesson completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,22 @@ import styled from 'styled-components'
 const SectionCaption = styled.p`
   font-weight: 600;
   font-size: 18px;
-  
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
 `
 
 const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 const IndexPage = () => (
