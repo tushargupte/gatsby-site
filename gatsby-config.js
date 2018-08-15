5a6fc264abd9f3af85c05220b78da1a1a22b8df1
tushargupte <tushar@TusharsMBP.local>
2018-08-15 14:54:05 +0530

    Installed Contentful and Added API Keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
     keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
   plugins: [
-    'gatsby-plugin-react-helmet'],
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceID: 'zjhj2zw7f6x8',
+        accessToken: '6b0b858cfb289e86a123b8dba9d8c77e36bf3a9079e9ad9f5c58c791b803ea1c'
+      }
+    }
+  ],
 }
